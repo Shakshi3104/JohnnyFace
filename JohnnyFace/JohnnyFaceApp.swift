e5c5d5354235c3e6b646208d3259f18613411285
Shakshi3104 <51497924+Shakshi3104@users.noreply.github.com>
2022-04-04 21:30:02 +0900

    Initial Commit

---
+++ JohnnyFace/JohnnyFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  JohnnyFaceApp.swift
+//  JohnnyFace
+//
+//  Created by MacBook Pro M1 on 2022/04/04.
+//
+
+import SwiftUI
+
+@main
+struct JohnnyFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
